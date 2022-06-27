a4421ae8f32f9f420945fe6ac5c218814fb3a0a5
sambhavkaushik <masterboy376@gmail.com>
2022-06-27 22:19:04 +0530

    fixed navbar and bg

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import { Context } from '../context/context'
